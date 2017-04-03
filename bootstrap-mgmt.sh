539c32cb6381d3ddf3b3e381a04cf92d81bc66ec
Yellaturi <Babu_Yellaturi@AMER.DELL.COM>
2017-04-03 20:16:17 +0530

    first commit

---
+++ bootstrap-mgmt.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+
+# copy examples into /home/vagrant (from inside the mgmt node)
+cp -a /vagrant/examples/* /home/vagrant
+chown -R vagrant:vagrant /home/vagrant
+
+# configure hosts file for our internal network defined by Vagrantfile
+cat >> /etc/hosts <<EOL
+
+# vagrant environment nodes
+10.0.15.10  mgmt
+10.0.15.11  lb
+10.0.15.21  web1
+10.0.15.22  web2
+10.0.15.23  web3
+10.0.15.24  web4
+EOL
